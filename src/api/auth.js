6fe73289df89002c3b7ea86d125ceeaf7335f394
kamikaza36 <petar.klenovic@gmail.com>
2020-11-06 17:49:40 +0100

    added auth login and singup to auth route

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const logger = require('winston');
 const { ErrorHandler } = require('../middleware/error-handler/error-handling/error-handler');
+const authServices = require('../services/auth/auth-services');
 
 const router = express.Router();
-const userServices = require('../services/user/user-services');
 
 /* Register user */
 router.post('/signup', (req, res, next) => {
@@ -14,7 +14,24 @@ router.post('/signup', (req, res, next) => {
     return res.json(new ErrorHandler(400, 'Invalid data. Please use email and password'));
   }
 
-  return userServices.userSignup(userData)
+  return authServices.authLogin(userData)
+    .then((userAuth) => res.json(userAuth))
+    .catch((err) => {
+      logger.error(`Error occured ${err.message}`);
+      return next(err);
+    });
+});
+
+/* Login user */
+router.post('/login', (req, res, next) => {
+  logger.debug('Recived req');
+  const userData = req.body;
+
+  if (!userData.email || !userData.password) {
+    return res.json(new ErrorHandler(400, 'Invalid data. Please use email and password'));
+  }
+
+  return authServices.authLogin(userData)
     .then((userAuth) => res.json(userAuth))
     .catch((err) => {
       logger.error(`Error occured ${err.message}`);
