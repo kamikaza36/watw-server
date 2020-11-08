834a65dfceaf0521f1602eb49754cd4bb7f19c49
kamikaza36 <petar.klenovic@gmail.com>
2020-11-08 10:37:46 +0100

    converted auth-services to async/await

--- src/services/auth/auth-services.js
+++ src/services/auth/auth-services.js
@@ -5,73 +5,67 @@ const jwt = require('jsonwebtoken');
 const { ErrorHandler } = require('../../middleware/error-handler/error-handling/error-handler');
 const userHelper = require('../user/user-helper');
 
-const authSignup = (userData) => {
-  const { email } = userData;
-  return bcrypt.hash(userData.password, 5)
-    .then((hashedPass) => {
-      return userHelper.createNewUser(email, hashedPass)
-        .then((response) => {
-          if (response.code === 11000) {
-            const [key, value] = Object.entries(response.keyValue)[0];
-            throw new ErrorHandler(409, `User with ${key} ${value} already exists!`);
-          }
-          const user = response._doc;
+const authSignup = async (userData) => {
+  try {
+    const { email } = userData;
+    const hashedPass = await bcrypt.hash(userData.password, 5);
+    const dbResponse = await userHelper.createNewUser(email, hashedPass);
+    if (dbResponse.code === 11000) {
+      const [key, value] = Object.entries(dbResponse.keyValue)[0];
+      throw new ErrorHandler(409, `User with ${key} ${value} already exists!`);
+    }
+    const user = dbResponse._doc;
 
-          if (user) {
-            if (!user.aUserId) {
-              logger.error(user.message);
-              throw new ErrorHandler(500, 'There was problem with server, try again later.');
-            }
-            const filteredUser = user;
-            delete filteredUser.password;
+    if (user) {
+      if (!user.aUserId) {
+        logger.error(user.message);
+        throw new ErrorHandler(500, 'There was problem with server, try again later.');
+      }
+      const filteredUser = user;
+      delete filteredUser.password;
 
-            const jwtToken = jwt.sign(
-              { id: user.id },
-              config.get('jwtSecret'),
-              { expiresIn: 3600 },
-            );
+      const jwtToken = jwt.sign(
+        { id: user.id },
+        config.get('jwtSecret'),
+        { expiresIn: 3600 },
+      );
 
-            return { jwtToken, filteredUser };
-          }
-          throw response;
-        })
-        .catch((err) => {
-          logger.error(err.message);
-          throw err;
-        });
-    });
+      return { jwtToken, filteredUser };
+    }
+    throw dbResponse;
+  } catch (err) {
+    logger.error(err.message);
+    throw err;
+  }
 };
 
-const authLogin = (loginData) => {
+const authLogin = async (loginData) => {
   const { email, password } = loginData;
 
-  return userHelper.getUserViaEmail(email)
-    .then((response) => {
-      if (!response) {
-        throw new ErrorHandler(401, 'Wrong email or password.');
-      }
-      const user = response._doc;
-      return bcrypt.compare(password, user.password)
-        .then((auth) => {
-          if (!auth) {
-            throw new ErrorHandler(401, 'Wrong email or password.');
-          }
-          const filteredUser = user;
-          delete filteredUser.password;
-
-          const jwtToken = jwt.sign(
-            { id: filteredUser.id },
-            config.get('jwtSecret'),
-            { expiresIn: 3600 },
-          );
-
-          return { jwtToken, filteredUser };
-        });
-    })
-    .catch((err) => {
-      logger.error(err);
+  try {
+    const dbResponse = await userHelper.getUserViaEmail(email);
+    if (!dbResponse) {
       throw new ErrorHandler(401, 'Wrong email or password.');
-    });
+    }
+    const user = dbResponse._doc;
+    const auth = await bcrypt.compare(password, user.password);
+    if (!auth) {
+      throw new ErrorHandler(401, 'Wrong email or password.');
+    }
+    const filteredUser = user;
+    delete filteredUser.password;
+
+    const jwtToken = jwt.sign(
+      { id: filteredUser.id },
+      config.get('jwtSecret'),
+      { expiresIn: 3600 },
+    );
+
+    return { jwtToken, filteredUser };
+  } catch (err) {
+    logger.error(err);
+    throw new ErrorHandler(401, 'Wrong email or password.');
+  }
 };
 
 const authServices = {
