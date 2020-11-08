da7c92f8b8174fec3256b5eca7ad4f34021c9ab5
kamikaza36 <petar.klenovic@gmail.com>
2020-11-08 09:20:06 +0100

    converted db-connect to async/await

--- db-connect.js
+++ db-connect.js
@@ -2,18 +2,19 @@ const logger = require('winston');
 const mongoose = require('mongoose');
 const config = require('config');
 
-const dbConnect = () => {
+const dbConnect = async () => {
   logger.info('Connecting to database');
-
-  mongoose.connect(config.get('mongoURI'), {
-    useNewUrlParser: true,
-    user: config.get('mongoUser'),
-    pass: config.get('mongoPass'),
-    useUnifiedTopology: true,
-  })
-    .catch((err) => {
-      logger.error(`Failed to connect to MongoDb. Error:${err}`);
+  try {
+    await mongoose.connect(config.get('mongoURI'), {
+      useNewUrlParser: true,
+      user: config.get('mongoUser'),
+      pass: config.get('mongoPass'),
+      useUnifiedTopology: true,
     });
+    logger.info('Successfully connected to database!');
+  } catch (error) {
+    logger.error(`Failed to connect to MongoDb. Error:${error}`);
+  }
 };
 
 module.exports = {
